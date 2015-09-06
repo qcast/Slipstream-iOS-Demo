311f810413a8d1e61baa04224fd5f418d0e8ea40
Vinnie Magro <vmagro@socaldevs.com>
2015-09-06 11:06:09 -0400

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Gallery""""""""""""""""""""""""""
    This reverts commit 01cd1ceb9929508d8c73db55d850e820a82820bf.

---
+++ My Cool App/PhotoCell.h
@@ -0,0 +1,17 @@
+//
+//  PhotoCell.h
+//  
+//
+//  Created by Vinnie Magro on 9/6/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import <AssetsLibrary/AssetsLibrary.h>
+
+@interface PhotoCell : UICollectionViewCell
+
+@property(nonatomic, strong) ALAsset *asset;
+
+@end
